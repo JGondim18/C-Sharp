f495ea1e69f3034dcf5529b614c321eecb09f5f6
JGondim18 <133431504+JGondim18@users.noreply.github.com>
2023-05-12 17:44:01 -0300

    Add files via upload

---
+++ Tipos de combustível/Tipos de combustível/Program.cs
@@ -0,0 +1,34 @@
+﻿int Alcool = 1;
+int Gasolina = 2;
+int Diesel = 3;
+int Fim = 4;
+int combustível= int.Parse(Console.ReadLine()); 
+
+
+while(combustível != 4 )
+{
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out combustível))
+
+        if (combustível == Alcool)
+    {
+        Console.WriteLine($"Alcool: {Alcool}");
+    }
+    else if(combustível == Gasolina)
+    {
+        Console.WriteLine($"Gasolina: {Gasolina}");
+    }
+    else if(combustível==Diesel)
+    {
+        Console.WriteLine($"Diesel: {Diesel}");
+    }
+    else if(combustível == Fim)
+    {
+        break;
+    }
+    else
+    {
+        Console.WriteLine("MUITO OBRIGADO");
+    }
+    
+}
