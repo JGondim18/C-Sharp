751eb15ca7db985082aac0014d574875e1b5e4da
JGondim18 <133431504+JGondim18@users.noreply.github.com>
2023-05-12 22:54:05 -0300

    Add files via upload
    Ao usuário definir as primeiras variáveis (N e X) e valores restantes, o programa terá definido um intervalo, e todos os números digitados após contribuem para a contagem de valores por dentro e por fora do intervalo.

---
+++ Intervalo de inteiros/Intervalo de inteiros/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Insira o valor N:");
+
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Insira o valor X:");
+
+int X = int.Parse(Console.ReadLine());
+
+if (N > X)
+{
+    Console.WriteLine("O primeiro valor deve ser menor que o segundo!");
+    Environment.Exit(0);
+}
+
+Console.WriteLine($"Intervalo: {N},{X}");
+
+int IN = 0;
+int OUT= 0;
+
+for(int i = 1; i <= N; i++)
+{
+    Console.WriteLine($"Insira o {i}° valor:");
+    int I = int.Parse(Console.ReadLine());
+
+    if(I > N && I < X)
+    {
+        IN++; 
+    }
+    else{ 
+        OUT++; 
+    }
+}
+
+Console.WriteLine($"{IN} por dentro do intervalo");
+Console.WriteLine($"{OUT} por fora do intervalo");
