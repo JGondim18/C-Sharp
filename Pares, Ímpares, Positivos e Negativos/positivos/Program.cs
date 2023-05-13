89cb41e9aa3dfed7e3d1e7595929a1c7dacd8d09
JGondim18 <133431504+JGondim18@users.noreply.github.com>
2023-05-12 22:35:46 -0300

    Add files via upload
    Este código realiza a leitura de diversos valores 'float' e inicia uma contagem por meio de condicionais, caso o valor seja par ou ímpar, positivo ou negativo, no final a saída exibe o resultado adquirido durante o programa.

---
+++ Pares, Ímpares, Positivos e Negativos/positivos/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.WriteLine("Bem vindo! Digite quantos valores deseja testar:");
+int contador = int.Parse(Console.ReadLine());
+
+int quantidadePositivos = 0;
+int quantidadeNegativos = 0;
+int quantidadePares = 0;
+int quantidadeImpares = 0;
+
+for (int i = 1; i <= 5; i++) 
+{
+    Console.Write($"Digite o {i}º valor: ");
+    float valor = float.Parse(Console.ReadLine());
+
+        if (valor > 0)
+    {
+        quantidadePositivos++;
+    }
+    else if(valor< 0)
+    {
+        quantidadeNegativos++;
+    }
+        if(valor % 2==0)
+    { 
+        quantidadePares++; 
+    }  
+    else { 
+        quantidadeImpares++; 
+    }
+}
+
+Console.WriteLine($"{quantidadePositivos} valor(es) positivo(s)");
+Console.WriteLine($"{quantidadeNegativos} valor(es) negativo(s)");
+Console.WriteLine($"{quantidadePares} valor(es) par(es)");
+Console.WriteLine($"{quantidadeImpares} valor(es) impar(es)");
+Console.WriteLine($"{quantidadePositivos} valor(es) positivo(s)");
